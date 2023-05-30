30ad206aca51cfb0bdba4e0a7db677f57893581a
yorandm <yoran.demunter@student.hogent.be>
2023-05-30 15:50:08 +0200

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import 'swiper/css';
 import 'swiper/css/bundle';
 import './globals.css'
-import Image from 'next/image'
 
 import { Inter } from 'next/font/google'
+import Link from 'next/link';
 // import style from './css/layout.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,7 +24,8 @@ export default function RootLayout({
         <main className="page">
       <div className="headerNav">
         <div className="header">
-          <Image src="/img/img_8685.jpg" alt="Picture of the Yoran Demunter" className="headerImg" />
+           {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/img/img_8685.jpg" alt="Picture of the Yoran Demunter" className="headerImg" />
           <div className="headerInfo">
             <h1>Yoran Demunter</h1>
             <p>{"Hi I'm Yoran. I am a Belgian software engineer student at HoGent, currently in my final year. Apart from that I am volountier at FabLab Erpe-Mere where I am responsible for the Youth part of the organisation. My other time consumer is WSC Dr. Tistaertvrienden VZW, we create cycling races in and around Affligem, my home town. There I'm responsible for the applications at the city, Belgian Cycling,... As well as the orginazation of two races, Abdijkoers voor dames and Affligem Classic. And last but not least, I love themeparks. I love them so much i work in one as studentjob. In Plopsaland De Panne I'm in the operations department. "}</p>
@@ -32,9 +33,9 @@ export default function RootLayout({
         </div>
         <nav className="nav">
           <ul>
-            <li data-selected="true"><a href="/aboutme" >About me</a></li>
-            <li data-selected="true"><a href="/projecten">Projects</a></li>
-            <li data-selected="true"><a href="/contact">Contact</a></li>
+            <li data-selected="true"><Link href="/aboutme" >About me</Link></li>
+            <li data-selected="true"><Link href="/projecten">Projects</Link></li>
+            <li data-selected="true"><Link href="/contact">Contact</Link></li>
           </ul>
         </nav>
 
