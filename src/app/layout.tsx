f24b79459e2d0318fd51456a59599103d922d8f6
yorandm <yoran.demunter@student.hogent.be>
2023-05-30 20:07:37 +0200

    foutjes rechtzetten

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           <img src="/img/img_8685.jpg" alt="Picture of the Yoran Demunter" className="headerImg" />
           <div className="headerInfo">
             <h1>Yoran Demunter</h1>
-            <p>{"Hi I'm Yoran. I am a Belgian software engineer student at HoGent, currently in my final year. Apart from that I am volountier at FabLab Erpe-Mere where I am responsible for the Youth part of the organisation. My other time consumer is WSC Dr. Tistaertvrienden VZW, we create cycling races in and around Affligem, my home town. There I'm responsible for the applications at the city, Belgian Cycling,... As well as the orginazation of two races, Abdijkoers voor dames and Affligem Classic. And last but not least, I love themeparks. I love them so much i work in one as studentjob. In Plopsaland De Panne I'm in the operations department. "}</p>
+            <p>{"Hi I'm Yoran. I am a Belgian software engineer student at HoGent, currently in my final year. Apart from that I am volountier at FabLab Erpe-Mere where I am responsible for the Youth part of the organisation. Another passion I have is WSC Dr. Tistaertvrienden VZW, we create cycling races in and around Affligem, my home town. There I'm responsible for the applications at the municipalities, Belgian Cycling,... As well as the orginazation of two races, 'Abdijkoers voor dames' and 'Affligem Classic'. And last but not least, I love themeparks. I love them so much that even I work in one as studentjob. In Plopsaland De Panne I'm in the operations department. "}</p>
           </div>
         </div>
         <nav className="nav">
